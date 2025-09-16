bb986a14bd7ba2c0aeb45e60164763b5e736bfb3
MatiasFill <rmscontato@gmail.com>
2025-09-16 14:08:25 -0300

    atual front

--- src/api/axios.cjs
+++ src/api/axios.cjs
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL do backend
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://front-estoque-ashy.vercel.app/';
 
 const api = axios.create({
   baseURL: API_URL,
