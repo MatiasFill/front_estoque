e9138423c44461f9a9cc2528dff88571bbe33fc4
MatiasFill <rmscontato@gmail.com>
2025-09-17 00:17:13 -0300

    ta brabo

--- src/api/axios.cjs
+++ src/api/axios.cjs
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL do backend
-const API_URL = import.meta.env.VITE_API_URL || 'https://estoqueback.vercel.app/';
+const API_URL = import.meta.env.VITE_API_URL || 'https://backcontrole.netlify.app/';
 
 const api = axios.create({
   baseURL: API_URL,
